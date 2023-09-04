6d4b4ca2f69b8841a4605c23f5d0afd53b198509
CHINAMI-G5QE8BO\Administrator <279846654@qq.com>
2023-09-04 14:51:26 +0800

    初始化

---
+++ src/settings.420cc.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/loading.fire",orientation:"",jsList:[],bundleVers:{internal:"4aef9",prefab:"06e19",resources:"e49e9",background:"22d8f",costume:"4de3b",makeup:"38ede",main:"83af7"}};
